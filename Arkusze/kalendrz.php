7c55e48da53205740fc8666fac6084b9e3e8b2fb
xFoReXx <72551350+xFoReXx@users.noreply.github.com>
2021-10-19 19:35:48 +0200

    Add files via upload
    Huj nie działa mi xampp nie wiem jak to wygląda

---
+++ Arkusze/kalendrz.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"  content="width=device-width,  initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <div class="gorny-lewy">
+        <img src="logo1.png" alt="KALENDARZ">
+    </div>
+    <div class="gorny-prawy">
+        <h1>Kalendarz</h1>
+        <p>Miesiąc: lipiec, rok:2021</p>
+    </div>
+<footer class="stopka">
+<form action="kalendrz.php" method="POST">
+<label for="wpis">Dodaj wpis:<input type="text" name="wydarzenie"><button type="submit">Dodaj</button></label> 
+<p>Mateusz</p>
+</form>
+</footer>
+</body>
+</html>
